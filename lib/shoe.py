dbde5050bc77c0216394c961a8f867627bc96024
antwoine <antwoinegriggs@outlook.com>
2023-07-10 20:39:12 -0400

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def is_interger(self, size):
+        if (type(size) is int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def get_size(self):
+        return self._size
+
+    def cobble(self, condition="new"):
+        self.condition = "New"
+        print(f"Your shoe is as good as {condition}!")
+
+    size = property(get_size, is_interger)
